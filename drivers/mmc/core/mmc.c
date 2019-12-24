ee60f031fa38687f2e11e117432933e827411883
freeza <freeza@gmail.com>
2019-12-24 13:34:16 -0800

    Revert "mmc: core: Allow BKOPS and CACHE ctrl even if no HPI support"
    This reverts commit e203e54a8b75bbc46da3bba06ce355780f78a4de.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -547,7 +547,8 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 			card->cid.year += 16;
 
 		/* check whether the eMMC card supports BKOPS */
-		if (ext_csd[EXT_CSD_BKOPS_SUPPORT] & 0x1) {
+		if (!mmc_card_broken_hpi(card) &&
+		    (ext_csd[EXT_CSD_BKOPS_SUPPORT] & 0x1) &&
 				card->ext_csd.hpi) {
 			card->ext_csd.bkops = 1;
 			card->ext_csd.man_bkops_en =
@@ -2203,7 +2204,7 @@ reinit:
 	 * the existence of cache and it can be turned on.
 	 * If HPI is not supported then cache shouldn't be enabled.
 	 */
-	if (card->ext_csd.cache_size > 0) {
+	if (!mmc_card_broken_hpi(card) && card->ext_csd.cache_size > 0) {
 		if (card->ext_csd.hpi_en &&
 			(!(card->quirks & MMC_QUIRK_CACHE_DISABLE))) {
 			err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
